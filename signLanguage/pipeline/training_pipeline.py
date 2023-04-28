842c9238d8c75f7f184621f7b1e59606ef97f15f
Atanu <atanukundu1991@gmail.com>
2023-04-28 20:22:27 +0530

    data validation added

--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -2,15 +2,17 @@ import sys, os
 from signLanguage.logger import logging
 from signLanguage.exception import SignException
 from signLanguage.components.data_ingestion import DataIngestion
+from signLanguage.components.data_validation import DataValidation
 
-from signLanguage.entity.config_entity import DataIngestionConfig
+from signLanguage.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
-from signLanguage.entity.artifact_entity import DataIngestionArtifact
+from signLanguage.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 class TrainingPipeline:
 
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
@@ -27,13 +29,37 @@ class TrainingPipeline:
 
             logging.info("Exited the start_data_ingestion method of Training Pipeline class")
 
+            return data_ingestion_artifact
+
         except Exception as e:
             raise SignException(e, sys)
+
+
+
+    def start_data_validation(self, data_ingestion_artifact:DataIngestionArtifact) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of Training Pipeline class")
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact, data_validationconfig=self.data_validation_config)
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info("Exited the start_data_validation method of Training Pipeline class")
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys)
+
 
     
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
 
         except Exception as e:
             raise SignException(e, sys)
