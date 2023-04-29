cb2e380c342e9517c0ef8d99b3b4ca3a777b73f7
Atanu <atanukundu1991@gmail.com>
2023-04-29 23:01:30 +0530

    app added

--- app.py
+++ app.py
@@ -1,5 +1,79 @@
+import sys, os
 from signLanguage.pipeline.training_pipeline import TrainingPipeline
+from signLanguage.exception import SignException
+from signLanguage.utils.main_utils import decodeImage, encodeImageIntoBase64
+from flask import Flask, request, jsonify, render_template, Response
+from flask_cors import CORS, cross_origin
+from signLanguage.constant.application import APP_HOST, APP_PORT
 
-obj = TrainingPipeline()
-obj.run_pipeline()
-print("Training Pipeline finished")
+# obj = TrainingPipeline()
+# obj.run_pipeline()
+# print("Training Pipeline finished")
+
+app = Flask(__name__)
+CORS(app=app)
+
+class ClientApp:
+    def __init__(self):
+        self.file_name = "inputImage.jpg"
+
+
+
+@app.route("/train")
+def trainRoute():
+    obj = TrainingPipeline()
+    obj.run_pipeline()
+    return "Training Successfully!!"
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=['POST', 'GET'])
+@cross_origin()
+def predictRoute():
+    try:
+        image = request.json['image']
+        decodeImage(image, clApp.file_name)
+
+        os.system("cd yolov5/ && python detect.py --weights my_model.pt --img 416 --conf 0.5 --source ../data/inputImage.jpg")
+
+        opencodebase64 = encodeImageIntoBase64("yolov5/runs/detect/exp/inputImage.jpg")
+        result = {"image": opencodebase64.decode('utf-8')}
+        os.system("rm -rf yolov5/runs")
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside json data")
+
+    except KeyError:
+        return Response("Key value error incorrect key password")
+
+    except Exception as e:
+        print(e)
+        result = "Invalid input"
+
+    return jsonify(result)
+
+
+
+@app.route("/live", methods=['GET'])
+@cross_origin()
+def predictLive():
+    try:
+        os.system("cd yolov5/ && python detect.py --weights my_model.pt --img 416 --conf 0.5 --source 0")
+        os.system("rm -rf yolov5/runs")
+        return "Camera Starting!!"
+
+    except ValueError as val:
+        print(val)
+        return Response("Value not found inside json data")
+
+
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    app.run(host=APP_HOST, port=APP_PORT)
